31bdecc7152743e68849f6176bf0d08bdcd6c931
LMDN <lmdn@novonordisk.com>
2021-07-16 14:03:20 +0200

    replaced hymn_id with doc_id in annotate script

--- data-raw/annotate.R
+++ data-raw/annotate.R
@@ -17,7 +17,7 @@ if (length(model_files) > 0) {
 if (!file.exists("data/annotated_hymns.rda")) {
   annotated_hymns <- udpipe_annotate(object = mdl,
                                      x = hymns$text,
-                                     doc_id = as.character(hymns$hymn_id)) %>%
+                                     doc_id = as.character(hymns$doc_id)) %>%
     as_tibble() %>%
     mutate(vowels = str_count(token, "a|e|i|o|u|y|æ|ø|å")) %>%
     mutate(across(c(doc_id, token_id, head_token_id), as.integer))
