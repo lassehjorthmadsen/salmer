85d059ede84798fdf1d620d275bf119b042d0461
Lasse Hjorth Madsen <lassehjorthmadsen@gmail.com>
2021-08-15 15:08:06 +0200

    minor work on snippets and vignettes

--- data-raw/snippets.R
+++ data-raw/snippets.R
@@ -1,32 +1,22 @@
 library(tidyverse)
 devtools::load_all()
 
-annotated_hymns <- annotated_hymns %>% mutate(across(c(doc_id, token_id, head_token_id), as.integer))
+hymns <- hymns %>% rename(doc_id = hymn_id)
+usethis::use_data(hymns, overwrite = TRUE)
+
+annotated_hymns <- annotated_hymns %>%
+  mutate(vowels = str_count(token, "a|e|i|o|u|y|æ|ø|å")) %>%
+  mutate(across(c(doc_id, token_id, head_token_id), as.integer))
+
+usethis::use_data(annotated_hymns, overwrite = TRUE)
 
 df <- annotated_hymns %>%
   select(doc_id, paragraph_id, token, upos)
 
-few_hymns <- hymns %>%
-  #filter(hymn_id < 51) %>%
-  select(hymn_id, text) %>%
-  group_by(hymn_id) %>%
-  mutate(paragraph_id = row_number()) %>%
-  rename(doc_id = hymn_id) %>%
-  ungroup()
+df <- df %>% mutate(ryhm = str_extract(token, "[aeiouyæøå][^aeiouyæøå]+$"))
 
-few_hymns2 <- collapse_annotation(df)
-
-comp <- few_hymns %>%
-  mutate(text = str_trim(text)) %>%
-  left_join(few_hymns2, by = c("doc_id", "paragraph_id")) %>%
-  replace_na(list(text.y = ""))
-
-comp %>% view()
-comp <- comp %>% mutate(lx = nchar(text.x), ly = nchar(text.y))
-comp %>% count(ly - lx)
-
-comp %>% count(text.x == text.y)
-comp %>% filter(text.x != text.y) %>% data.frame()
-comp %>% filter(str_detect(text.x, "' "))
-comp %>% filter(str_detect(text.x, "1"))
-
+df %>%
+  filter(!is.na(ryhm)) %>%
+  slice_sample(n = 10) %>%
+  left_join(select(df, token, ryhm), by = "ryhm") %>%
+  slice_sample(n = 10)
