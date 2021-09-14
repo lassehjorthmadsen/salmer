69dc47ddedb8adc38a6f3b2974cf65902e320e85
LMDN <lmdn@novonordisk.com>
2021-09-14 20:37:28 +0200

    work on rhyme_scheme()

--- R/rhyme_scheme.R
+++ R/rhyme_scheme.R
@@ -3,6 +3,9 @@
 #' @param ref_id
 #' @param df tibble based on output from \code{udpipe::udpipe_annotate()}
 #' similar to the \code{annotated_hymns} dataset.
+#' @param pron A tibble assumed to contain a \code{token},
+#' a \code{stress_vowel} and a \code{remainder} column, all
+#' coded in then phonetic alphabet. Passed on to \code{get_rhymes()}.
 #'
 #' @return a tibble with rhyme structure in new column, \code{scheme}. The
 #' number, x, in that column indicates that the word must rhyme with the x-
@@ -14,17 +17,17 @@
 #' cut_up(annotated_hymns)
 #'
 #' @export
-rhyme_scheme <- function(ref_id = 1, df) {
+rhyme_scheme <- function(ref_id = 1, df, pron) {
 
   song <- df %>%
-    dplyr::rowid_to_column("row_id") %>%
+    tibble::rowid_to_column("row_id") %>%
     dplyr::filter(doc_id == ref_id)
 
   last_words <- song %>%
     dplyr::filter(upos != "PUNCT") %>%
     dplyr::group_by(paragraph_id) %>%
     dplyr::slice_tail(n = 1) %>%
-    dplyr::mutate(rhymes = list(get_rhymes(token, df))) %>%
+    dplyr::mutate(rhymes = list(get_rhymes(token, pron))) %>%
     dplyr::ungroup()
 
   rhymes <- last_words %>%
@@ -38,7 +41,10 @@ rhyme_scheme <- function(ref_id = 1, df) {
                                             lag3 %in% unlist(rhymes) ~ 3,
                                             lag3 %in% unlist(rhymes) ~ 4,
                                             TRUE ~ NA_real_)) %>%
-    dplyr::select(row_id, scheme)
+    dplyr::ungroup() %>%
+    dplyr::mutate(tok_row = row_number() - scheme,
+                  must_rhyme = ifelse(is.na(tok_row), NA_character_, token[tok_row])) %>%
+    dplyr::select(row_id, scheme, tok_row, must_rhyme)
 
   song %>%
     dplyr::left_join(rhymes, by = "row_id") %>%
